2cf7114e0e3735eb4dd36f9810536c91eb676a57
christopherjneelankavil <christopherneelankavil@gmail.com>
2023-12-22 00:06:32 +0530

    first commit

---
+++ lib/buttons_list.dart
@@ -0,0 +1,25 @@
+class Btn{
+  static const String delete="D";
+  static const String clear="C";
+  static const String percent="%";
+  static const String multiply="X";
+  static const String divide="/";
+  static const String add="+";
+  static const String subtract="-";
+  static const String ans="=";
+  static const String dec=".";
+
+  static const String num0="0";
+  static const String num1="1";
+  static const String num2="2";
+  static const String num3="3";
+  static const String num4="4";
+  static const String num5="5";
+  static const String num6="6";
+  static const String num7="7";
+  static const String num8="8";
+  static const String num9="9";
+
+
+
+}
---
+++ lib/calculator_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({super.key});
+
+  @override
+  State<CalculatorScreen> createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            //o/p
+            Expanded(
+              child: SingleChildScrollView(
+                child: Container(
+                  alignment: Alignment.bottomRight,
+                  padding:const  EdgeInsets.all(16),
+                  child:const Text("0",
+                    style: TextStyle(
+                      fontSize: 48,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.end,
+                  ),
+                ),
+              ),
+            ),
+
+            //i/p
+            const Wrap(
+              children: [
+
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
